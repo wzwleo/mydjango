aefeefa8b69bffb5c95b7d2f4df273ee95860a16
wzwleo <leo20050312@gmail.com>
2026-06-02 15:34:12 +0800

    Delete mblog directory

--- mblog/mysite/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from mysite.models import Post
-
-
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'pub_date')
-
-admin.site.register(Post, PostAdmin)
